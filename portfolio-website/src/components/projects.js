1a5f3006c242c973411615a82b6a9772893a8f5c
David Kumar <david.dn.kumar@gmail.com>
2021-07-13 19:06:57 -0500

    update styling for projects component

--- portfolio-website/src/components/projects.js
+++ portfolio-website/src/components/projects.js
@@ -6,7 +6,7 @@ const ProjectCard = ({ img, title, description1, description2, github, website }
     return (
         <div className="h-full flex mb-16 items-center justify-center">
 
-            <div className="flex flex-col justify-center w-4/5 lg:w-2/5">
+            <div className="flex flex-col justify-center w-4/5 lg:w-3/5">
                 <h1 className="font-bold text-3xl sm:text-3xl">{title}</h1>
                 <div>
                     <p className="ml-1 font-serif text-gray-500 my-5">{description1}</p>
