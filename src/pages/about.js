9c03f8afd55d1dc9d7b251e59767fe0f7f548bb9
David K <67809795+davidk101@users.noreply.github.com>
2022-04-25 07:35:54 -0500

    remove the about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -22,7 +22,7 @@ const About = ({ setPage, pageVariants, pageTransition }) => {
                     </h1>
                     <p className="sm:ml-1 text-base mt-6 sm:mt-12 sm:text-xl font-serif text-gray-500">
                         <p className = "my-5">
-                            Hey there! I am an AWS-Certified, five-time tech intern with several apps that have amassed hundreds of users - one of which was acquired in August'21 by a company with $1,000,000,000 in assets.
+                            
                         </p>
                     </p>
 
